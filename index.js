9d078a9c3e8d7ac89415ea59faeeec0d50430c0d
Matias Perez Hughes <hughesmatias@gmail.com>
2020-04-30 22:52:58 -0300

    init socket io and send message when somebody is connection

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
   res.send('Hi world!');
+});
+
+io.on("connection", socket => {
+ //socket is client what is connection
+ socket.emit("messages", {
+   message: `a message ... ${new Date().getMilliseconds()}`
+ })
 })
 
 server.listen(8080, () => {
